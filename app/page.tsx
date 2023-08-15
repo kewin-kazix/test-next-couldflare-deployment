232bd723ccd140ffde030d79f9dc21398e099b49
kewin@kazix.io <kewinf271@gmail.com>
2023-08-15 12:39:42 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+export const runtime = 'edge';
+
+async function getData() {
+  const res = await fetch(
+    "https://dev-xxiy8iynl1hazsp.api.raw-labs.com/json-programming-heroes"
+  );
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
+export default async function Home() {
+  const games = (await getData()) as any[];
+
+  return <pre>{JSON.stringify(games)}</pre>;
+}
